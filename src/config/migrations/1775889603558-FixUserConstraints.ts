a5e49ebd65f764ef1ffcb4d282ecb688ee5357b2
akhtarhameed521 <akhtar.hameed521@gmail.com>
2026-04-13 02:01:37 +0500

    chore(migration) issue fixed

--- src/config/migrations/1775889603558-FixUserConstraints.ts
+++ src/config/migrations/1775889603558-FixUserConstraints.ts
@@ -1,18 +1,18 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+// import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class FixUserConstraints1775889603558 implements MigrationInterface {
-    name = 'FixUserConstraints1775889603558'
+// export class FixUserConstraints1775889603558 implements MigrationInterface {
+//     name = 'FixUserConstraints1775889603558'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "employeeId" DROP NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "image" character varying(255)`);
-    }
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "employeeId" DROP NOT NULL`);
+//         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
+//         await queryRunner.query(`ALTER TABLE "users" ADD "image" character varying(255)`);
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD "image" character varying(20)`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "employeeId" SET NOT NULL`);
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "image"`);
+//         await queryRunner.query(`ALTER TABLE "users" ADD "image" character varying(20)`);
+//         await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "employeeId" SET NOT NULL`);
+//     }
 
-}
+// }
