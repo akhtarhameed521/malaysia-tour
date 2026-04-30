dba275d3081efc99198567d8f32c763e6543213e
akhtarhameed521 <akhtar.hameed521@gmail.com>
2026-04-30 13:53:44 +0500

    chore(room) everything is fine

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { RoomService } from "./room.service";
 import { asyncHandler } from "../common/helper/async-handler.helper";
+import { ApiError } from "../common/helper/api-error.helper";
+import { statusCode } from "../common/messages/status-code.messages";
 
 export class RoomController {
     private roomService: RoomService;
@@ -31,6 +33,15 @@ export class RoomController {
 
     delete = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
         const result = await this.roomService.deleteRoom(Number(req.params.id));
+        res.status(result.statusCode).json(result);
+    });
+
+    bulkUpload = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+        const file = req.file;
+        if (!file) {
+            throw new ApiError(statusCode.BadRequest, "No file uploaded");
+        }
+        const result = await this.roomService.bulkUploadRooms(file.buffer);
         res.status(result.statusCode).json(result);
     });
 }
